b457105294d37c0c751163ab0d8cc49d32969702
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-12-16 15:48:21 +0300

    Added close $ menu resource

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,11 @@
 import logo1 from './logo1.png'
 import logo2 from './logo2.png'
+import menu from  './menu.svg'
+import close from  './close.svg'
 
 export {
     logo1,
-    logo2
+    logo2,
+    menu,
+    close
 };
