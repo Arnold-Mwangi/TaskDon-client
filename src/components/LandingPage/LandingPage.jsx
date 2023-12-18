bfd3c24f7dc869c944b7b450e4c46fdbeeac4924
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-12-18 16:27:14 +0300

    Added mobile view landing image

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -5,18 +5,28 @@ import Navbar from '../Navbar/Navbar'
 
 const LandingPage = () => {
     return (
-        <div className='overflow-hidden'>
-            <div>
-                <Navbar />
-            </div>
-            <div className='container w-full flex items-center py-5 mx-auto'>
-    
+        <div className='w-full overflow-hidden relative'>
+
+            <div className='container flex items-center py-5 max-w-7xl mx-auto'>
+
 
                 <img
                     alt=""
-                    aria-hidden="true"                    
-                    className=" md:hidden w-full h-auto mb-3 max-w-7xl mx-auto absolute top-20"
+                    aria-hidden="true"
+                    className=" md:hidden block  w-full h-auto mb-3 max-w-7xl mx-auto "
                     src="https://github.githubassets.com/assets/hero-mobile-7163f4f5de41.webp"
+                />
+
+
+                <div></div>
+                <img
+                    alt=""
+                    aria-hidden="true"
+                    width="2236"
+                    height="1630"
+                    className="absolute h-auto pointer-events-none hidden md:block "
+                    style={{ width: 'min(1118px, max(100vh, 100vw))', left: '67%' }}
+                    src="https://github.githubassets.com/assets/hero-desktop-a38b0fd77b6c.webp"
                 />
 
             </div>
@@ -24,15 +34,7 @@ const LandingPage = () => {
     )
 }
 
+
 export default LandingPage
 
 
-        //  {/* <img
-        //             alt=""
-        //             aria-hidden="true"
-        //             // width="2236"
-        //             // height="1630"
-        //             className="absolute top-0 h-auto   sm: block"
-        //             style={{ width: 'min(118px, max(100vh, 100vw))', left: '50%' }}
-        //             src="https://github.githubassets.com/assets/hero-desktop-a38b0fd77b6c.webp"
-        //         /> */}
