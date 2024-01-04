20be632870230ada09bbc4bf18fba3b208cfe09d
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-01-04 22:55:17 +0300

    Added form input fields as array

---
+++ src/components/Authentication/Resources/resources.js
@@ -0,0 +1,24 @@
+const forms = [
+    {
+        label: 'Email',
+        placeholder: 'Enter your Email',
+        name: 'Email',
+    },
+    {
+        label: 'Password',
+        placeholder: 'Enter your Password',
+        name: 'Password',
+    },
+    {
+        label: 'Confirm Password',
+        placeholder: 'Confirm your Password',
+        name: 'Confirm Password',
+    },
+    {
+        label: 'WhatsApp Number',
+        placeholder: 'Enter your WhatsApp Number',
+        name: 'WhatsappNumber',
+    },
+];
+
+export default forms
