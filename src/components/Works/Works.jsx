4e520eacbff61388dca9d4ef1ffb27077fa33523
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-01-12 15:37:16 +0300

    added site work with a timeline tracker

---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,219 @@
+import React from "react";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css'
+// import {textVariant} from '../utils/motion'
+
+
+const Works = () => {
+    return (
+
+        <VerticalTimeline>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work "
+                contentStyle={{ background: '#12181f', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                date="Availaible"
+                dateStyle = {{color: "violet-gradient"}}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            // icon={<WorkIcon />}
+            >
+                <h3 className="vertical-timeline-element-title">TaskDon </h3>
+                <h4 className="vertical-timeline-element-subtitle">Works Department</h4>
+                <div className="flex flex-row flex-wrap gap-4">
+                    <p className="green-text-gradient">
+                        IT
+                    </p>
+                    <p className="pink-text-gradient">
+                        Programming
+                    </p>
+
+                    <p className="blue-text-gradient">
+                        Social-Sciences
+                    </p>
+
+                    <p className="orange-text-gradient">
+                        Law
+                    </p>
+
+                    <p >
+                        others...
+                    </p>
+                </div>
+
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work "
+                contentStyle={{ background: '#12181f', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                date="Availaible"
+                dateStyle = {{color: "violet-gradient"}}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            // icon={<WorkIcon />}
+            >
+                <h3 className="vertical-timeline-element-title">TaskDon </h3>
+                <h4 className="vertical-timeline-element-subtitle">Works Department</h4>
+                <div className="flex flex-row flex-wrap gap-4">
+                    <p className="green-text-gradient">
+                        IT
+                    </p>
+                    <p className="pink-text-gradient">
+                        Programming
+                    </p>
+
+                    <p className="blue-text-gradient">
+                        Social-Sciences
+                    </p>
+
+                    <p className="orange-text-gradient">
+                        Law
+                    </p>
+
+                    <p >
+                        others...
+                    </p>
+                </div>
+
+            </VerticalTimelineElement>
+
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work "
+                contentStyle={{ background: '#12181f', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                date="Availaible"
+                dateStyle = {{color: "violet-gradient"}}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            // icon={<WorkIcon />}
+            >
+                <h3 className="vertical-timeline-element-title">TaskDon </h3>
+                <h4 className="vertical-timeline-element-subtitle">Works Department</h4>
+                <div className="flex flex-row flex-wrap gap-4">
+                    <p className="green-text-gradient">
+                        IT
+                    </p>
+                    <p className="pink-text-gradient">
+                        Programming
+                    </p>
+
+                    <p className="blue-text-gradient">
+                        Social-Sciences
+                    </p>
+
+                    <p className="orange-text-gradient">
+                        Law
+                    </p>
+
+                    <p >
+                        others...
+                    </p>
+                </div>
+
+            </VerticalTimelineElement>
+
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work "
+                contentStyle={{ background: '#12181f', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                date="Availaible"
+                dateStyle = {{color: "violet-gradient"}}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            // icon={<WorkIcon />}
+            >
+                <h3 className="vertical-timeline-element-title">TaskDon </h3>
+                <h4 className="vertical-timeline-element-subtitle">Works Department</h4>
+                <div className="flex flex-row flex-wrap gap-4">
+                    <p className="green-text-gradient">
+                        IT
+                    </p>
+                    <p className="pink-text-gradient">
+                        Programming
+                    </p>
+
+                    <p className="blue-text-gradient">
+                        Social-Sciences
+                    </p>
+
+                    <p className="orange-text-gradient">
+                        Law
+                    </p>
+
+                    <p >
+                        others...
+                    </p>
+                </div>
+
+            </VerticalTimelineElement>
+
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work "
+                contentStyle={{ background: '#12181f', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                date="Availaible"
+                dateStyle = {{color: "violet-gradient"}}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            // icon={<WorkIcon />}
+            >
+                <h3 className="vertical-timeline-element-title">TaskDon </h3>
+                <h4 className="vertical-timeline-element-subtitle">Works Department</h4>
+                <div className="flex flex-row flex-wrap gap-4">
+                    <p className="green-text-gradient">
+                        IT
+                    </p>
+                    <p className="pink-text-gradient">
+                        Programming
+                    </p>
+
+                    <p className="blue-text-gradient">
+                        Social-Sciences
+                    </p>
+
+                    <p className="orange-text-gradient">
+                        Law
+                    </p>
+
+                    <p >
+                        others...
+                    </p>
+                </div>
+
+            </VerticalTimelineElement>
+
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work "
+                contentStyle={{ background: '#12181f', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                date="Availaible"
+                dateStyle = {{color: "violet-gradient"}}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            // icon={<WorkIcon />}
+            >
+                <h3 className="vertical-timeline-element-title">TaskDon </h3>
+                <h4 className="vertical-timeline-element-subtitle">Works Department</h4>
+                <div className="flex flex-row flex-wrap gap-4">
+                    <p className="green-text-gradient">
+                        IT
+                    </p>
+                    <p className="pink-text-gradient">
+                        Programming
+                    </p>
+
+                    <p className="blue-text-gradient">
+                        Social-Sciences
+                    </p>
+
+                    <p className="orange-text-gradient">
+                        Law
+                    </p>
+
+                    <p >
+                        others...
+                    </p>
+                </div>
+
+            </VerticalTimelineElement>
+
+        </VerticalTimeline>
+
+    )
+}
+
+export default Works
