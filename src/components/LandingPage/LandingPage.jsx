40d506b9304a5314b7b776f10445ce2664e09a3a
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-01-15 16:53:59 +0300

    Overlayed big screen image

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,43 +1,37 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import Hero from '../Hero/Hero'
-
-
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Hero from '../Hero/Hero';
 
 const LandingPage = () => {
     return (
         <div className='w-full overflow-hidden'>
             <Navbar />
-            <div className='relative container flex items-center py-5 max-w-7xl mx-auto h-auto'>
-
-
-                <img
-                    alt=""
-                    aria-hidden="true"
-                    className=" md:hidden block  w-full  mb-3 max-w-7xl mx-auto "
-                    src="https://github.githubassets.com/assets/hero-mobile-7163f4f5de41.webp"
-                />
-
-
+            <div className='container relative'>
+                <div className='relative'>
                
-                <img
-                    alt=""
-                    aria-hidden="true"
-                    width="2236"
-                    height="1630"
-                    className="absolute h-screen pointer-events-none max-w-7xl hidden md:block "
-                    style={{ width: 'min(1118px, max(100vh, 100vw))', left: '67%' }}
-                    src="https://github.githubassets.com/assets/hero-desktop-a38b0fd77b6c.webp"
-                />
 
+                    <img
+                        alt=""
+                        aria-hidden="true"
+                        className="md:hidden block w-full mb-3 max-w-7xl mx-auto z-20"
+                        src="https://github.githubassets.com/assets/hero-mobile-7163f4f5de41.webp"
+                    />
+
+                    <img
+                        alt=""
+                        aria-hidden="true"
+                        width="2236"
+                        
+                        className="absolute pointer-events-none h-[400px] hidden md:block z-0"
+                        style={{ width: 'min(1118px, max(100vh, 100vw))', left: '67%' }}
+                        src="https://github.githubassets.com/assets/hero-desktop-a38b0fd77b6c.webp"
+                    />
+                </div>
             </div>
 
             <Hero />
         </div>
-    )
-}
+    );
+};
 
-
-export default LandingPage
-
-
+export default LandingPage;
