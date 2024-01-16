33b58c9fecdec7121590f2ff872dc52a064a741b
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-01-16 16:57:32 +0300

    g

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [react()],
+// })
+export default defineConfig ({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@react-three/fiber': 'react-three-fiber'
+    }
+  }
 })
