47c3991133cb85efbea23e30242a616659a492a2
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-01-26 16:29:59 +0300

    Traditional select method

--- src/components/Authentication/Form.jsx
+++ src/components/Authentication/Form.jsx
@@ -1,99 +1,78 @@
 import React from "react";
 
+const RenderForm = ({ currentStep, forms, formData, handleInputChange, handleFormSubmit }) => {
+  const form = forms[currentStep];
 
-
-const RenderForm = ( currentStep, forms, formData, handleInputChange, handleContinue, handleFormSubmit ) => {
-
-    const form = forms[currentStep];
-
-    return (
-        <div className='flex flex-col'>
-            {Array.from(Array(currentStep)).map((_, index) => (
-                <React.Fragment key={index}>
-
-
-                    <span>&nbsp;</span>
-
-                    <div className="relative z-0 w-full mb-5 group">
-                        <input
-
-                            name={forms[index].name}
-                            id={form.name}
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                            placeholder={forms[index].placeholder}
-                            onChange={handleInputChange}
-                            value={formData[forms[index].name] || ''}
-                        />
-
-                    </div>
-                </React.Fragment>
-            ))}
-            {currentStep < forms.length && (
-                <>
-                    <div className='md: flex md: items-end justify-between gap-2 '>
-                        <div className=' '>
-
-
-                            <div className="relative z-0 w-full mb-5 group">
-                                {form.type === 'select' ? (
-                                    <select
-
-                                        name={form.name}
-                                        id={form.name}
-                                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                        onChange={handleInputChange}
-                                        value={formData[form.name] || ''}
-                                    >
-                                        {form.options.map((option) => (
-                                            <option key={option} value={option}>
-                                                {option}
-                                            </option>
-                                        ))}
-                                    </select>
-                                ) : (
-                                    <input
-                                        type={form.type}
-                                        name={form.name}
-                                        id={form.name}
-                                        className="block text-center py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-
-                                        required={form.type !== 'select'}
-                                        onChange={handleInputChange}
-                                        value={formData[form.name] || ''}
-                                    />
-                                )}
-                                <label
-                                    htmlFor={form.name}
-                                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                                >
-                                    {form.label}
-                                </label>
-
-                            </div>
-
-                        </div>
-
-                        <div className='text-[#098776] ' >
-                            {currentStep !== forms.length - 1 ?
-
-                                <input
-                                    type='button'
-                                    name='continue'
-                                    value='continue'
-                                    className='cursor-pointer ml-5 py-2.5 px-0'
-                                    onClick={handleContinue}
-                                /> : ''
-                            }
-                        </div>
-
-
-                        {console.log(currentStep)}
-                    </div>
-
-                </>
-            )}
+  return (
+    <div className="flex flex-col">
+      {forms.map((form, index) => (
+        <div key={index} className="relative z-0 w-full mb-5 group">
+          {form.type === "select" ? (
+            <div className="relative">
+              <select
+                name={form.name}
+                id={form.name}
+                className="block w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                onChange={(e) => handleInputChange(e, form.name, form.type === "select" ? e.target.value : null)}
+                required={form.fields[0].required}
+                value={formData[form.name] || ""}
+                
+              >
+                <option value="" disabled>
+                Select an option
+                </option>
+                {form.options.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <svg
+                  className="w-5 h-5 text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+              </div>
+            </div>
+          ) : (
+            <input
+              type={form.type}
+              name={form.name}
+              id={form.name}
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              required={form.fields[0].required}
+              onChange={(e) => handleInputChange(e, form.name, form.type === "select" ? e.target.value : null)}
+              value={formData[form.name] || ""}
+            />
+          )}
+          <label
+            htmlFor={form.name}
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            {form.label}
+          </label>
         </div>
-    );
+      ))}
+      <div className="mt-4">
+        <input
+          type="submit"
+          name="actionButton"
+          value="Submit"
+          className="px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RenderForm;
