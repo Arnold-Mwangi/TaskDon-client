b363687507172e22b67afd9749aa5883edf827e3
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-01-26 22:55:00 +0300

    login using fomkit

---
+++ src/components/Authentication/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AuthContext } from "./AuthContext/AuthContext"; 
+
+
+export const Login = () => {
+
+    return (
+
+        <div className=" gradient-form">
+
+        </div>
+    )
+
+}
