641f454d5fb1d1fb1255086b58cfd44b95982ad6
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-01-26 16:30:26 +0300

    added select and options to form object

--- src/components/Authentication/Resources/resources.js
+++ src/components/Authentication/Resources/resources.js
@@ -2,34 +2,73 @@ const forms = [
     {
         label: 'Email',
         placeholder: 'Enter your Email',
-        name: 'Email',
+        name: 'email',
         type: 'email',
+        fields: [
+            {
+                label: 'Email',
+                name: 'Email',
+                type: 'email',
+                required: true,
+            },
+        ],
     },
     {
         label: 'Password',
         placeholder: 'Enter your Password',
         name: 'Password',
         type: 'password',
+        fields: [
+            {
+                label: 'Password',
+                name: 'Password',
+                type: 'password',
+                required: true,
+            },
+        ],
     },
     {
         label: 'Confirm Password',
         placeholder: 'Confirm your Password',
         name: 'Confirm Password',
         type: 'password',
+        fields: [
+            {
+                label: 'Confirm Password',
+                name: 'Confirm Password',
+                type: 'password',
+                required: true,
+            },
+        ],
     },
     {
         label: 'WhatsApp Number',
         placeholder: 'Enter your WhatsApp Number',
         name: 'WhatsappNumber',
         type: 'number',
+        fields: [
+            {
+                label: 'WhatsApp Number',
+                name: 'WhatsappNumber',
+                type: 'number',
+                required: true,
+            },
+        ],
     },
     {
         name: 'role',
         placeholder: 'Role',
         type: 'select',
-        options: ['STUDENT', 'TASKER']
-        
+        options: ['STUDENT', 'TASKER'],
+        fields: [
+            {
+                label: 'Role',
+                name: 'role',
+                type: 'select',
+                required: true,
+            },
+        ],
     },
 ];
 
-export default forms
+export default forms;
